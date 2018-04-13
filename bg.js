4b428381a9a4630837f04cc9c73563fbf41ec941
Stefan Markovic <zmstefan@gmail.com>
2018-04-14 00:16:50 +0200

    monitor only inactive tabs

--- bg.js
+++ bg.js
@@ -1,6 +1,5 @@
 chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
-    console.log(changeInfo);
-    if(!!tab.url && tab.url.includes('web.freshchat.com') && !!changeInfo.title && changeInfo.title.includes('(')){
+    if(!!tab.url && tab.url.includes('web.freshchat.com') && !!changeInfo.title && changeInfo.title.includes('(') && !tab.active){
         var myAudio = new Audio(); // create the audio object
         myAudio.src = "tone.mp3"; // assign the audio file to its src
         myAudio.play(); // play the tone
